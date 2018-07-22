019ffd74b363fd64d2c906f27527a8fc3be675d3
Ignacio R Espino <irespino@capozios.com>
2018-07-22 14:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
+<<<<<<< HEAD
 var greetSpecialGuest = `Hello, ${specialGuest}!`;
 
 var topic = `Let's talk about space.`;
+=======
+var greetSpecialGuest = "Hello" + specialGuest + "!";
+
+var topic = "Let's talk about space.";
+>>>>>>> a6cf34ea35c65a62a2d54ae3c6b9efedbb77f80c
 
 var conversation = `Let's talk about ${topic}.`;
